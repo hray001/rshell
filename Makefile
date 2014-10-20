677a6c0f97188e9df9952f8b796750a56e88834e
Harrison Ray <hray001@ucr.edu>
2014-10-20 00:52:56 -0700

    created base files

---
+++ Makefile
@@ -0,0 +1,6 @@
+
+all
+    
+
+rshell
+    
---
+++ main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string.h>
+#include <cstdlib>
+#include <errno.h>
+#include <stdio.h>
+
+using namespace std;
+
+int main(){
+
+    string input = "";
+    string prompt = "$ ";
+    cout << prompt;
+    cin >> input;
+
+    strtok(input);
+
+
+
+
+
+
+    return 0;
+}
